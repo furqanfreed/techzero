b5faad977d50cc57ea716bdd6d4051961a24d576
Furqan Freed <furqan@adwora.com>
2025-12-11 11:52:42 +0100

    register telescope only for local env

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,13 @@
 <?php
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
 ];
+
+// Only register Telescope in local environment
+if (app()->environment('local')) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
